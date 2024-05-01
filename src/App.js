1f0b9f56885a7cd92b4c40e42336cc4bf05f432e
Krishna <102092559+krishna9754@users.noreply.github.com>
2024-05-01 01:15:35 -0700

    add context

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css'
 import Data from './components/movieList.json'
 import { useState } from 'react';
+import { counterContext } from './context/Context'
+import FilterListning from './components/FilterListning'
 import ButtonFilter from './components/ButtonFilter';
-import FilterListning from './components/FilterListning';
-import { counterContext } from './context/Context';
 
 function App() {
-
   const [item, setItem] = useState(Data)
+  const [isVisible, setVisible] = useState(false)
   const menuGenreItems = [...new Set(Data.map((dataGenreItem) => dataGenreItem.moviegenres))]
   const menuLanguageItems = [...new Set(Data.map((dataLanguageItem) => dataLanguageItem.movielanguages))]
   const filterGenreItem = (genres) => {
@@ -23,12 +23,12 @@ function App() {
 
 
   return (
+    <counterContext.Provider value={{item, menuGenreItems, menuLanguageItems, filterGenreItem, filterLanguageItem, setItem, isVisible, setVisible, Data}}>
     <div className='bg-black'>
-      <counterContext.Provider value={{menuGenreItems, menuLanguageItems, filterGenreItem, filterLanguageItem, setItem, item}}>
-        <ButtonFilter />
-        <FilterListning item={item} />
-      </counterContext.Provider>
+      <ButtonFilter/>
+      <FilterListning/>
     </div>
+    </counterContext.Provider>
   );
 }
 
