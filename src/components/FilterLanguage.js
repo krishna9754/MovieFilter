e644bfbf1f0bd0859ccb2538f6d2b142fa97b3f6
Krishna <102092559+krishna9754@users.noreply.github.com>
2024-05-01 02:19:09 -0700

    Update FilterLanguage.js

--- src/components/FilterLanguage.js
+++ src/components/FilterLanguage.js
@@ -1,10 +1,9 @@
 
 import React, { useContext, useState } from 'react'
-import Data from './movieList.json'
 import { counterContext } from '../context/Context';
 
 
-export default function FilterLanguage({ menuLanguageItems, setItem, filterLanguageItem }) {
+export default function FilterLanguage() {
     const value = useContext(counterContext)
     const dublicateLanguageData = removeDuplicatesLanguage(value.menuLanguageItems);
     const [selected, setSelected] = useState()
@@ -26,9 +25,9 @@ export default function FilterLanguage({ menuLanguageItems, setItem, filterLangu
                 <div className="row row-cols-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-6 bg-black pb-2 mb-3 border-b-2">
                     {
                         dublicateLanguageData.map((languageVal, index) => (
-
+                            
                             <form >
-                                <input type='checkbox' checked={value.isVisible} onClick={() => handleLanguageClick(index, languageVal)} />
+                                <input type='checkbox' onClick={() => handleLanguageClick(index, languageVal)} />
                                 <label className='text-white'> {languageVal}</label>
                             </form>
                         ))
